efa4ab772a7a2286fc62e6c761196cdca70b7f27
johnzennin <umutsah461@gmail.com>
2026-06-16 00:29:12 +0300

    Delete sql_injection_analyzer directory

--- sql_injection_analyzer/parsers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-SQL Injection Security Analysis and Audit Platform
-Parsers Package Initialization
-"""
-
-from .log_parser import LogParser
-
-__all__ = ['LogParser']
--- sql_injection_analyzer/reporters/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-SQL Injection Security Analysis and Audit Platform
-Reporters Package Initialization
-"""
-
-from .report_generator import ReportGenerator
-
-__all__ = ['ReportGenerator']
